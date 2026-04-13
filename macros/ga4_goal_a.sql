ee23a5da1ff54885df94ef5802d9c1300777f218
Peter Wang <pw33537515@gmail.com>
2026-04-13 16:56:50 +1200

    remove ga4 cleanup

--- macros/ga4_goal_a.sql
+++ macros/ga4_goal_a.sql
@@ -150,6 +150,13 @@ remove_outdated_data AS (
      date between DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) and CURRENT_DATE()
      AND  ABS(DATE_DIFF(DATE(report_end_date), CURRENT_DATE(), DAY)) >=2
   )
+),
+delete_outdated_data AS (
+  DELETE FROM {{ source(source_name, table_name) }} WHERE
+    WHERE (
+     date between DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) and CURRENT_DATE()
+     AND  ABS(DATE_DIFF(DATE(report_end_date), CURRENT_DATE(), DAY)) >=2
+  )
 )
 SELECT * 
 FROM remove_outdated_data 
